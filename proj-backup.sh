f64ecf9340454387c63b3f751642d43069d4342a
absolute-cinema-jpg <4lex99@googlemail,com>
2025-11-12 12:00:26 +0000

    script will notify when done, created a launchd file to run automatically

--- proj-backup.sh
+++ proj-backup.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 project_location=/Volumes/IS_Nexis/IN_STARLAND
 backup_location=/Users/ff_tc_25006/Documents/Backup
 project_folder_name=IN_STARLAND
@@ -6,3 +8,5 @@ rsync -avPh $project_location $backup_location
 zip -r $backup_location/$(date +%y%m%d)_$project_folder_name.zip $backup_location/$project_folder_name
 mkdir $backup_location/trash
 mv $backup_location/$project_folder_name $backup_location/trash/$(date +%y%m%d)_$project_folder_name
+
+osascript -e 'display notification "Backup finished successfully!" with title "Backup Job" sound name "Glass"'
