99f3303c1af8644ea765c9650ac54ea701e094f7
absolute-cinema-jpg <4lex99@googlemail,com>
2025-11-12 19:25:20 +0000

    shit's kinda fucked. Figuring it out. Trying to get it working with launchd

--- proj-backup.sh
+++ proj-backup.sh
@@ -4,9 +4,16 @@ project_location=/Volumes/IS_Nexis/IN_STARLAND
 backup_location=/Users/ff_tc_25006/Documents/Backup
 project_folder_name=IN_STARLAND
 
+#Copy the whole folder to backup location (ideally Documents)
 rsync -avPh $project_location $backup_location
+
+#Archive the freshly copied folder
 zip -r $backup_location/$(date +%y%m%d)_$project_folder_name.zip $backup_location/$project_folder_name
-mkdir $backup_location/trash
+
+#Ensure trash dir exists
+mkdir -p $backup_location/trash
+
+#Move the source folder into trash with a unique timestamp
 mv $backup_location/$project_folder_name $backup_location/trash/$(date +%y%m%d)_$project_folder_name
 
 osascript -e 'display notification "Backup finished successfully!" with title "Backup Job" sound name "Glass"'
